0a55c154445bc17d8b06caa17b4b6d5ea3fa6cdb
Artsiom_Berazavik <Artsiom_Berazavik@epbyminw3766.minsk.epam.com>
2016-11-17 17:59:33 +0300

    Enought for today.

--- Salad/src/Obstract/Vegetable.java
+++ Salad/src/Obstract/Vegetable.java
@@ -1,17 +1,29 @@
 package Obstract;
- //@date Nov 17, 2016
- //@author Artsiom_Berazavik
+//@date Nov 17, 2016
+//@author Artsiom_Berazavik
 
 public abstract class Vegetable {
+
     private String name;
-    private int massa;
+    int weight; //private final question to mentor
     private int calorificValue;
     private double price;
-    private boolean requiredHeadTratment;
-    
-    // разобрать геттеры и сеттеры еще раз. 
-    public String getName () {
+    private boolean requiredHeatTratment;
+
+    public String getName() {
         return name;
-    } 
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public Vegetable(int weight) {
+        this.weight = weight;
+    }
+    
+    public double CalcCalorificValue() {
+        System.out.println("Calculating calories in 100g");
+        return 0.0;
+    }
 }
-//moving in OOP branch
